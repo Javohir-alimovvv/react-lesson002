253e411640b2ba6d6edcea956ef48997a1fa94f3
javohir <ajavohir969@gmail.com>
2024-10-25 11:33:53 +0500

    loyiha

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer_bg">
+            <p className="footer_item">Copyright @ 2022 | Mohid</p>
+        </div>
+    )
+}
+
+export default Footer
